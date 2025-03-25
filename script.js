3c6591bb4e78b8fc68ab1db084dd781088a7e510
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2025-03-25 17:17:05 +0530

    add getFetchData and getBookData function to fetch and get the data from api

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-alert("I am working")
+//Function to get the books from api
+const getFetchData = async () => {
+  const url =
+    "https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%252Cid%252Cetag%252CvolumeInfo&query=tech";
+  const options = { method: "GET", headers: { accept: "application/json" } };
 
+  try {
+    // Fetch the url with option and hold it in a response variable
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Response Status : ${response.status}`);
+    }
+    // Convert the response to a json object and hold it on a data variable
+    const data = await response.json();
+    // console.log(data);
+    return data;
+  } catch (error) {
+    throw new Error(`Response Status : ${response.status}`);
+  }
+};
+
+const getBookData = async () => {
+  try {
+    // const bookData = await getFetchData();
+    const { data } = await getFetchData();
+    console.log(data.data[0].volumeInfo.title);
+    return data;
+  } catch (error) {
+    throw new Error("Book Data not found", error);
+  }
+};
+
+
+getBookData();
+// console.log(bookData);
